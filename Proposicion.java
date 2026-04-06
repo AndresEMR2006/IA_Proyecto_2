c94da6e4de12516d59928f17e00abee6060fca47
David Piñeros <dpineros05@gmail.com>
2026-04-06 12:05:15 -0500

    Refactor Proposicion class and add methods

--- Proposicion.java
+++ Proposicion.java
@@ -1,29 +1,43 @@
+import java.util.Map;
+
+/**
+ * Representa un literal o proposición atómica (ej. Romano(X) o ¬Romano(Marco)).
+ */
 public class Proposicion {
 
-    // Atributos de la clase Proposicion
     public String nombre;
     public Boolean negado;
     public String[] argumento;
 
-    // Constructor de la clase Proposicion
     public Proposicion(String nombre, Boolean negado, String[] argumento) {
         this.nombre = nombre;
         this.negado = negado;
         this.argumento = argumento;
     }
 
-    public Proposicion(){
+    public Proposicion(){}
+
+    /**
+     * Aplica la sustitución de variables encontrada en la unificación.
+     */
+    public Proposicion aplicarSustitucion(Map<String, String> sustitucion) {
+        String[] nuevoArgumento = new String[argumento.length];
+        for (int i = 0; i < argumento.length; i++) {
+            // Si el argumento está en el mapa, lo sustituye; si no, lo deja igual
+            nuevoArgumento[i] = sustitucion.getOrDefault(argumento[i], argumento[i]);
+        }
+        return new Proposicion(this.nombre, this.negado, nuevoArgumento);
+    }
+
+    /**
+     * Retorna la representación en String de la proposición.
+     */
+    public String imprimirString() {
+        String negacion = this.negado ? "¬" : "";
+        return negacion + this.nombre + "(" + String.join(", ", this.argumento) + ")";
     }
 
     public void imprimir(){
-        String negacion;
-
-        if(this.negado){
-            negacion = "¬";
-        }else{
-            negacion = "";
-        }
-        System.out.print(negacion + this.nombre + "(" + this.argumento + ") ");
+        System.out.print(imprimirString() + " ");
     }
-
 }
